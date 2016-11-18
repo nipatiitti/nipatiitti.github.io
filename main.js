3d5faaf33e7fe695287e7c544fcbe7f08cb96c20
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2016-11-18 17:39:56 +0200

    Fixing IE supports

--- main.js
+++ main.js
@@ -4,28 +4,36 @@ function myFunction() {
 
 var en = true;
 var fin = false;
-var lang = sessionStorage.getItem('lang');
+var lang = localStorage.getItem('lang');
 
 window.onload = function showPopUp() {
     var visited = sessionStorage.getItem('visited');
     if (!visited) {
       alert("You are going to get few alerts now. Pls read them carefully");
-      alert("Use chrome or firefox to show this site. DO NOT USE INTERNET EXPLORER");
+      if (!document.all) {
+        alert('Good job you are not using internet explorer. Lucky for you');
+      }
       alert("This site is optimized for halfscreen and mobile");
       alert("Oh and this site uses your sessionStorage (Not cookies) so you don't see these alert more than 1 time per session");
       alert("To show these again close and open this tab again");
       alert("Thank you for your attention and co-operating :D");
       sessionStorage.setItem('visited', true);
-      sessionStorage.setItem('lang', true);
+      localStorage.setItem('lang', true);
     }
-    if (lang == fin) {
+}
+
+function check() {
+    if (document.all === true) {
+        alert('We strongly recommend using good browser like google chrome instead of this shitty one');
+      }
+    if (!lang) {
         kaanto();
     }
-    if (layoutEngine.vendor === 'ie' && layoutEngine.version === 11){
-        document.getElementById("IE10").classList.toggle("paalle");
+    if (Function('/*@cc_on return document.documentMode===10@*/')()){
+        document.documentElement.className+=' ie10';
     }
-    if (layoutEngine.vendor === 'ie' && layoutEngine.version === 10){
-        document.getElementById("IE10").classList.toggle("paalle");
+    if (Function('/*@cc_on return document.documentMode===11@*/')()){
+        document.documentElement.className+=' ie10';
     }
 }
 
@@ -63,10 +71,10 @@ window.onclick = function(event) {
 
 function langToggle() {
     kaanto();
-    if (lang === en) {
-        sessionStorage.setItem('kieli', false);
+    if (!lang) {
+        localStorage.setItem('kieli', true);
     }
-    else if (lang === fin) {
-        sessionStorage.setItem('kieli', true);
+    else {
+        localStorage.setItem('kieli', false);
     }
 }
