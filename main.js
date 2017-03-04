a7ded8fefccfaebdb81970c3dded673d9ed9bce9
Niilo Jaakkola <Niilo.jaakkola@icloud.com>
2017-03-04 16:15:18 +0200

    Adding the site

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(ev) {
+  $('#code').click(function(){
+     window.location.href='coding/code.html';
+  })
+
+  $('#info').click(function(){
+     window.location.href='info/info.html';
+  })
+
+  $('#card').click(function(){
+     window.location.href='contact/contact.html';
+  })
+
+  $('#mortar').click(function(){
+     window.location.href='degree/degree.html';
+  })
+});
